4b5278a000c021847718900b82256d563a1a22aa
Passavit_Gunn <passavit1@gmail.com>
2023-08-27 17:58:40 +0700

    finish learning C# from w3school

--- program.cs
+++ program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿
 
-namespace HelloWorld
+namespace MyApplication
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                int[] myNumbers = new int[4 ]{ 1, 2, 3 ,4};
+                Console.WriteLine(myNumbers[2]);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+       
     }
 }
---
+++ program2.cs
@@ -0,0 +1,14 @@
+
+
+namespace MyApplication
+{
+   enum Level 
+   {
+    Easy, 
+    Medium , 
+    Hard
+   }
+    
+}
+
+
